b9ef7abe48523b3725238fab4e6d66cc9e16e580
Nagarjuna Myla <chasethenag420@gmail.com>
2015-11-01 14:18:33 -0700

    Change in task1 option4

--- Phase2/Task1_decoder.py
+++ Phase2/Task1_decoder.py
@@ -7,12 +7,12 @@ def main():
   width=10
   height=10
   fileSuffix=".mp4"
-  videoFileName=raw_input("Enter the video file name:\n")
   videoDir = raw_input("Enter the video file directory:\n")
+  videoFileName=raw_input("Enter the video file name:\n")  
   optionNumber=raw_input("Enter the option number:\n")
   fullPath = r'{0}\{1}'.format(videoDir,videoFileName+fileSuffix)
   inputFileName=r'{0}_{1}.tpc'.format(videoFileName,optionNumber)
-  outputFileName=r'{0}_{1}_out.mov'.format(videoFileName,optionNumber)
+  outputFileName=r'{0}_{1}_out{2}'.format(videoFileName,optionNumber,fileSuffix)
 
   inFile = open( inputFileName ) 
   frames= None
@@ -26,8 +26,6 @@ def main():
       frames=tpcDecodingOption3(inFile)
     elif optionNumber=='4':
       frames=tpcDecodingOption4(inFile)
-    elif optionNumber=='5':
-      frames=tpcDecodingOption5(inFile)
     else: 
       print "Input not valid"
       quit()
@@ -47,9 +45,14 @@ def decodeVideo(frames,fullPath,width,height,outputVideoFileName,inputFileName):
   fourcc=-1
 
   cap = cv2.VideoCapture(fullPath)
+  outVideoFile=None
   if cap.isOpened:
-    frameRate=cap.get(cv2.CAP_PROP_FPS)
-    fourcc=cap.get(cv2.CAP_PROP_FOURCC)
+    if cv2.__version__=='3.0.0':
+      frameRate=cap.get(cv2.CAP_PROP_FPS)
+      fourcc=cap.get(cv2.CAP_PROP_FOURCC)
+    else:
+      frameRate=cap.get(cv2.cv.CV_CAP_PROP_FPS)
+      fourcc=cap.get(cv2.cv.CV_CAP_PROP_FOURCC)
 
   outputFileName=inputFileName+"decoded.txt"
   outfile = open( outputFileName, 'w' )
@@ -128,54 +131,36 @@ def tpcDecodingOption4(inFile):
   for line in inFile:
     encodedSignal = list(map(float,line.split()))
     decodedSignal=[]
-    for index,value in enumerate(encodedSignal):
-      if index<=2:
-        decodedSignal.append(value)
+    for k,value in enumerate(encodedSignal):
+      if k<=1:
+        decodedSignal.append(int(round(encodedSignal[k])))
       else:
-        k1k2Diff = decodedSignal[index-1] - decodedSignal[index-2]
-        
-        if k1k2Diff == 0:
+        if k<=3:
           alpha1=0.5
           alpha2=0.5
+          decodedSignal.append(int(round(value+ alpha1*decodedSignal[k-1]  + alpha2*decodedSignal[k-2])))
         else:
-          alpha1 = (value - decodedSignal[index-2])/float(k1k2Diff)
-          alpha2 = (decodedSignal[index-1] - value)/float(k1k2Diff)
-
-        if alpha1>1 and alpha2>1:
-          alpha1=0.5
-          alpha2=0.5
-        elif alpha1<0 and alpha2<0:
-          alpha1=0.5
-          alpha2=0.5
-        elif alpha1<0 or alpha2>1:
-          alpha1=0.0
-          alpha2=1.0
-        elif alpha1 >1 or alpha2<0:
-          alpha1=1.0
-          alpha2=0.0
-        
-        decodedSignal.append(value+ alpha1*decodedSignal[index-1]  + alpha2*decodedSignal[index-2])
-
-    if lines==None:     
-      lines=np.array(decodedSignal)
-
-    else:
-      lines=np.column_stack((lines,decodedSignal))
-  return lines
-  
-def tpcDecodingOption5(inFile):
-  lines=None
-  alpha1=float(raw_input("Enter aplha1 predictor: "))
-  alpha2=float(raw_input("Enter aplha2 predictor: "))
-  for line in inFile:
-    encodedSignal = list(map(float,line.split()))
-    decodedSignal=[]
-    for index,value in enumerate(encodedSignal):
-      if index<=2:
-        decodedSignal.append(int(round(value)))
-      else:
-     
-        decodedSignal.append(int(round(value+ alpha1*decodedSignal[index-1]  + alpha2*decodedSignal[index-2])))
+          k1k2Diff = decodedSignal[k-2] - decodedSignal[k-4]
+          if k1k2Diff == 0:
+            alpha1=0.5
+            alpha2=0.5
+          else:
+            alpha1 = (decodedSignal[k-1] + decodedSignal[k-2]-decodedSignal[k-3]-decodedSignal[k-4])/float(k1k2Diff)
+            alpha2 = 1-alpha1
+          
+            if alpha1>1 and alpha2>1:
+              alpha1=0.5
+              alpha2=0.5
+            elif alpha1<0 and alpha2<0:
+              alpha1=0.5
+              alpha2=0.5
+            elif alpha1<0 or alpha2>1:
+              alpha1=0.0
+              alpha2=1.0
+            elif alpha1 >1 or alpha2<0:
+              alpha1=1.0
+              alpha2=0.0
+          decodedSignal.append(int(round(value+ alpha1*decodedSignal[k-1]  + alpha2*decodedSignal[k-2])))
 
     if lines==None:     
       lines=np.array(decodedSignal)
