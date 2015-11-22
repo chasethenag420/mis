fb7ee59a6b5f0f42f1f9b1a19c4e96b2831c883b
Nagarjuna Myla <chasethenag420@gmail.com>
2015-11-21 19:41:43 -0700

    Integrating task3 and task1b

--- Phase3/Task1a.py
+++ Phase3/Task1a.py
@@ -1,43 +1,22 @@
-__author__ = 'Nagarjuna'
 import cv2
-import sys
 import numpy as np
-import os
-import matplotlib.pyplot as plt
 import collections
 from sys import platform as _platform
-
-def main():
-  width=8
-  height=8
-  if _platform == "linux" or _platform == "linux2":
-    slash = '/'
-  elif _platform == "darwin":
-    slash = '/'
-  elif _platform == "win32":
-    slash = '\\'
-  fileSuffix=".mp4"
-  #videoDir = raw_input("Enter the video file directory:\n")
-  #videoFileName = raw_input("Enter the video file name:\n")
-  #numOfBits=int(raw_input("Enter number of bits:\n"))
-  numOfBits=8
-  videoDir=r'F:\ASU_Projects\MIS\mis\Phase1\sampleDataP1'
-  videoFileName='6'
-  fullPath = '{0}{2}{1}'.format(videoDir,videoFileName+fileSuffix,slash)
-  outFileName='{0}_hist_{1}.hst'.format(videoFileName,numOfBits)
-  extract_video_portion(fullPath,width,height,numOfBits,outFileName)
+__author__ = 'Nagarjuna'
 
 
-def quantize(yChannel,numOfBits,frameId,blocksCoordinate,outfile):
+def quantize(yChannel, numOfBits, frameId, blocksCoordinate, outfile):
 
     numOfPartitions = 2 ** numOfBits
     signal= yChannel.tolist()[0]
     #signal=[1,2,3,4,5,6,7,8,9]
-    maxValue = max(signal)
-    minValue = min(signal)
+    #maxValue = max(signal)
+    #minValue = min(signal)
+    maxValue=255
+    minValue=0
     partitionSize = (maxValue - minValue)/float(numOfPartitions)
     partitions=None
-    if partitionSize != 0 :
+    if partitionSize != 0:
       partitions = np.arange(minValue, maxValue+partitionSize, partitionSize)
     else :
       partitions = np.ones(numOfPartitions+1) * minValue
@@ -45,7 +24,7 @@ def quantize(yChannel,numOfBits,frameId,blocksCoordinate,outfile):
     binIndexes=np.digitize(np.array(signal),partitions)
     representative=[]
     for value in range(1,len(partitions)):
-      representative.append(partitions[value-1]/float(2)+partitions[value]/float(2))
+      representative.append(int(partitions[value-1]/2+partitions[value]/2))
 
     quantized=[]
 
@@ -58,10 +37,14 @@ def quantize(yChannel,numOfBits,frameId,blocksCoordinate,outfile):
       quantized.append(int(representative[index]))
 
     frequency_list=collections.Counter(quantized)
+    if len(frequency_list.keys()) != len(representative):
+      for i in representative:
+        if i not in frequency_list.keys():
+          frequency_list[i]=0
 
-    for idx,value in enumerate(frequency_list.keys()):
+    for idx, value in enumerate(frequency_list.keys()):
       freq_val = frequency_list[value]
-      outfile.write('{0},{1},{2},{3}'.format(frameId,blocksCoordinate,value,freq_val))
+      outfile.write("{0},{1},{2},{3}\n".format(frameId, blocksCoordinate, value, freq_val))
 
 
 
@@ -107,4 +90,24 @@ def extract_video_portion(fullPath,width,height,numOfBits,outFileName):
 
   outfile.close()
 
-main()
+if __name__ == "__main__":
+  width=8
+  height=8
+  if _platform == "linux" or _platform == "linux2":
+    slash = '/'
+  elif _platform == "darwin":
+    slash = '/'
+  elif _platform == "win32":
+    slash = '\\'
+  fileSuffix=".mp4"
+  videoDir = raw_input("Enter the video file directory:\n")
+  videoFileName = raw_input("Enter the video file name:\n")
+  numOfBits=int(raw_input("Enter number of bits:\n"))
+
+  #numOfBits=1
+  #videoDir=r'F:\ASU_Projects\MIS\mis\Phase3\reducedSizeVideo'
+  #videoFileName='R1'
+
+  fullPath = '{0}{2}{1}'.format(videoDir, videoFileName+fileSuffix, slash)
+  outFileName='{0}_hist_{1}.hst'.format(videoFileName,numOfBits)
+  extract_video_portion(fullPath, width, height, numOfBits, outFileName)
